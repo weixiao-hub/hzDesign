f986f1dc97f16b10d5b427c1486b1fc381e98728
weixiao <5261292+chz8866@user.noreply.gitee.com>
2023-06-06 02:06:06 +0800

    第一次提交 2023/6/6

---
+++ src/views/Home.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="Home">
+    <header>
+    <div class="logo">
+      <span>hz-design</span>
+    </div>
+    <div class="navbar">
+      <div class="item">设计</div>
+      <div class="item">开发</div>
+      <div class="item">组件</div>
+      <div class="item"></div>
+      <div class="item"></div>
+
+    </div>
+  </header>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'hzHome',
+  props: {
+    msg: String
+  }
+})
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+  #Home {
+    header {
+      display: flex;
+      justify-content: space-between;
+      height: 50px;
+      border-bottom: 1px solid #eee;
+      box-shadow: 0 0 5px #eee;
+    }
+    .navbar {
+      display: flex;
+    }
+  }
+</style>
