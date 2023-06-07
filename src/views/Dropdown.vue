4613ba08d09d8165599001f978862f978de3aed8
weixiao <5261292+chz8866@user.noreply.gitee.com>
2023-06-07 15:36:18 +0800

    sdfsdf

--- src/views/Dropdown.vue
+++ src/views/Dropdown.vue
@@ -7,7 +7,7 @@ defineComponent({
 <template>
   <div>
    <div class="top-title">
-    <p class="hz-title">Dropdown 下拉菜单</p>
+    <p class="hz-title">Dropdown 下拉菜单aaaa</p>
     <p>页面上的命令过多时，可将备选命令收纳到向下展开的浮层容器中。通常用于菜单下拉。</p>
    </div>
     <div class="case-card">
