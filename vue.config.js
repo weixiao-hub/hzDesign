edaeacf9e653074dfd037e6298191312092bdcc0
weixiao <5261292+chz8866@user.noreply.gitee.com>
2023-06-22 04:26:25 +0800

    别名配置

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,12 @@
 // vue.config.js
+const path = require('path');
 module.exports = {
   configureWebpack: {
     plugins: [
       require('unplugin-vue-components/webpack')({ /* options */ }),
     ],
   },
+  chainWebpack:(config) => {
+    config.resolve.alias.set('@',path.resolve(__dirname,'src'))
+  }
 }
